395d9b76a46cc73b80027621faec53654d495002
DaeHyeon <kayuaao96@gmail.com>
2022-07-29 13:18:38 +0900

    22.07.29 7.0 To Do List part one

--- src/App.js
+++ src/App.js
@@ -1,73 +1,41 @@
 import { useState, useEffect } from "react";
 
-/*
-  React는 컴포넌트가 삭제될 때 원하는 코드를 실행시킬 수 있다.
-  컴포넌트가 첫 번째 실행(원하는 타이밍, 생성될 때) => useEffect
-  컴포넌트가 삭제될 때 => Cleanup Function 방법 사용
-*/
-
-function Hello(){
-  //useEffect 두 번째 요소에 아무것도 없다는 것은 원하는 Func를 한 번만 실행시키고 싶다는 것.
-  useEffect(() => {
-    console.log("Created :)");
-    //함수(함수)의 반환값 지정 => 컴포넌트가 삭제될 때 반환된다.(Cleanup Function)
-    return () => console.log("destroyed :(");
-  }, []);
-  // function destroyedFunction(){
-  //   console.log("bye :(");
-  // }
-  // function effectFunction(){
-  //   console.log("created :)");
-  //   return destroyedFunction; // 컴포넌트의 반환값으로 원하는 Func을 지정하면 컴포넌트가 파괴될 때 실행된다.
-  // }
-  // useEffect(effectFunction, []);
-  return <h1>Hello</h1>
-}
-
 function App(){
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]); // 배열 지정
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){ // Input 태그가 비어있다면
+      return;
+    }
+    setToDo("");
+    /*
+      const food = [1, 2, 3, 4]; 
+      food 안에 있는 value를 6과 합하려면..
+      const arr = [6, food] <- 이는 에러 발생. (배열 안에 배열)
+      이를 해결하기 위해서는 ... 사용
+      const arr = [6, ...food] <- 성공! (6, 1, 2, 3, 4)
+    */
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
   return (
     <div>
-      {/* showing이 true라면 Hello 컴포넌트 출력, false라면 null 반환*/}
-      {showing ? <Hello/> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos {(toDos.length)}</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange} 
+          value={toDo} 
+          type="text" 
+          placeholder="Write your to do..."/>
+        <button>Add To Do</button>
+      </form>
     </div>
-  );
+
+  )
 }
 
 export default App;
-
-// const [counter, setValue] = useState(0);
-//   const [keyword, setKeyword] = useState("");
-//   const onClick = () => setValue((prev) => prev + 1);
-//   const onChange = (event) => {
-//     setKeyword(event.target.value);
-//   }
-//   useEffect(() =>{
-//     console.log("I run only once.");
-//   }, []);
-//   useEffect(() => {
-//     // keyword가 빈 칸이 아니거나, keyword의 길이가 5보다 클 때 호출
-//     // if(keyword !== "" && keyword.length > 5){
-//     //   console.log("Search for ", keyword);
-//     // }
-//     console.log("I run when 'keyword' changes");
-//   }, [keyword]); // state인 keyword가 변경될 때 위 조건문이 실행된다.
-//   useEffect(() => {
-//     console.log("I run when 'counter' changes");
-//   }, [counter]);
-//   useEffect(() => {
-//     console.log("I run when keyword & counter change");
-//   }, [keyword, counter]);
-//   return (
-//     <div>
-//       <input 
-//         value={keyword}
-//         onChange={onChange} 
-//         type="text" 
-//         placeholder="Search here..."/>
-//       <h1>{counter}</h1>
-//       <button onClick={onClick}>Click me</button>
-//     </div>
-//   );
