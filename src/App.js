95c58e5c39d032f771154a4eb08161e0635711aa
DaeHyeon <kayuaao96@gmail.com>
2022-07-30 18:40:23 +0900

    22.07.30 Coin Tracker add

--- src/App.js
+++ src/App.js
@@ -1,58 +1,38 @@
 import { useState, useEffect } from "react";
 
 function App(){
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]); // 배열 지정
-  const onChange = (event) => {
-    setToDo(event.target.value);
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === ""){ // Input 태그가 비어있다면
-      return;
-    }
-    setToDo("");
-    /*
-      const food = [1, 2, 3, 4]; 
-      food 안에 있는 value를 6과 합하려면..
-      const arr = [6, food] <- 이는 에러 발생. (배열 안에 배열)
-      이를 해결하기 위해서는 ... 사용
-      const arr = [6, ...food] <- 성공! (6, 1, 2, 3, 4)
-    */
-    setToDos((currentArray) => [toDo, ...currentArray]);
-  };
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  // coins의 경우 기본값을 지정하지 않으면 undefined 에러 발생
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    // 해당 경로로 request를 보내고
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    // 해당 api에 대한 응답으로 reponse를 받으면 이 오브젝트를 json으로 치환한 뒤
+    .then((response) => response.json())
+    // 바꾼 json 오브젝트를 useState의 coins로 할당한다.
+    .then((json) => {
+      setCoins(json);
+      // 코인을 성공적으로 가져왔으니 loading의 값을 false로 바꾼다.
+      setLoading(false);
+    });
+  }, []); // 1번만 실행시키기 위해 두 번째 요소를 비어놓는다.
   return (
     <div>
-      <h1>My To Dos {(toDos.length)}</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-          onChange={onChange} 
-          value={toDo} 
-          type="text" 
-          placeholder="Write your to do..."/>
-        <button>Add To Do</button>
-      </form>
-      <hr/>
+      <h1>The Coins! ({coins.length})</h1> 
+      {loading ? <strong>Loading...</strong> : null}
       <ul>
-        {
-          // map은 array에 있는 요소들을 모두 지정한 item으로 바꾼다.
-          // 함수도 가능!
-          // map함수의 첫 번째 요소는 배열의 첫 번째 아이템이다.
-          // ex) ['one', 'two'].map((item) => item.toUpperCase())
-          // => ['ONE', 'TWO]
-          // 이는 컴포넌트를 반환할 수 있다.
-          // toDos.map((item) => <li>{item}</li>)
-          // 해당 코드에는 문제가 존재하는데,
-          // 같은 컴포넌트의 list를 render할 때 key라는 prop을 요구한다.
-          // 이는 다음과 같이 바꿀 수 있다.
-          toDos.map((item, index) => <li key={index}>{item}</li>)
-          // map함수의 두 번째 요소는 배열의 각 아이템 위치를 말한다.
-          // 이를 key으로 넣어 고유(유니크) prop을 설정한다.
-        }
+        {coins.map((coin) => (
+          // key 할당을 위해 두 번째 요소를 사용할 필요 X
+          // 이유는 호출한 api에 코인 별 id가 존재하기 때문
+          <li>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD</li>
+        ))}
       </ul>
     </div>
   )
 }
 
 export default App;
+
+// 실시간 Coin 가격 정보를 가져오기 위해
+// coinpaprika.com의 API 사용
+// api.coinpaprika.com/v1/tickers
